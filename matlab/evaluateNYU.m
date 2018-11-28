b20455b2dcce1ff983b07b84c77914247e2d9b45
lpqdao <37049175+lpqdao@users.noreply.github.com>
2018-11-28 00:17:02 -0600

    Use beta25 instead of beta20

--- matlab/evaluateNYU.m
+++ matlab/evaluateNYU.m
@@ -1,31 +1,34 @@
+  GNU nano 2.2.6                                                                                                                                                                                                                                                                            File: evaluateNYU.m
+
 function evaluateNYU
 
-% Evaluation of depth prediction on NYU Depth v2 dataset. 
+% Evaluation of depth prediction on NYU Depth v2 dataset.
 
 % -------------------------------------------------------------------------
 % Setup MatConvNet
 % -------------------------------------------------------------------------
 
 % Set your matconvnet path here:
-matconvnet_path = '../../matconvnet-1.0-beta20';
+matconvnet_path = '~/matconvnet-1.0-beta25';
 setupMatConvNet(matconvnet_path);
 
 % -------------------------------------------------------------------------
 % Options
 % -------------------------------------------------------------------------
 
-opts.dataDir = fullfile(pwd, 'NYU');    % working directory
+opts.dataDir = fullfile(pwd, 'NYU');    % working directory,
 opts.interp = 'nearest';                % interpolation method applied during resizing
 
-netOpts.gpu = true;     % set to true to enable GPU support      
-netOpts.plot = true;    % set to true to visualize the predictions during inference          
+netOpts.gpu = true;     % set to true to enable GPU support
+netOpts.plot = true;    % set to true to visualize the predictions during inference
 
 % -------------------------------------------------------------------------
 % Prepate data
 % -------------------------------------------------------------------------
+%
 
-imdb = get_NYUDepth_v2(opts);
-net = get_model(opts);
+imdb = get_NYUDepth_v2(opts); %groundtruth depth?
+net = get_model(opts); %getting model?
 
 % Test set
 testSet.images = imdb.images(:,:,:, imdb.set == 2);
@@ -33,23 +36,27 @@ testSet.depths = imdb.depths(:,:, imdb.set == 2);
 
 % Prepare input for evaluation through the network, in accordance to the
 % way the model was trained for the NYU dataset. No processing is applied
-% to the ground truth. 
+% to the ground truth.
 meta = net.meta.normalization;  % information about input
-res = meta.imageSize(1:2) + 2*meta.border; 
+res = meta.imageSize(1:2) + 2*meta.border;
 testSet.images = imresize(testSet.images, res, opts.interp);  % resize
 testSet.images = testSet.images(1+meta.border(1):end-meta.border(1), 1+meta.border(2):end-meta.border(2), :, :);  % center crop
 
 % -------------------------------------------------------------------------
-% Evaluate network 
+% Evaluate network
 % -------------------------------------------------------------------------
+
+
+
+
 
 % Get predictions
 predictions = DepthMapPrediction(testSet, net, netOpts);
 predictions = squeeze(predictions); % remove singleton dimensions
 predictions = imresize(predictions, [size(testSet.depths,1), size(testSet.depths,2)], 'bilinear'); %rescale
 
-% Error calculation 
-errors = error_metrics(predictions, testSet.depths, []);
+% Error calculation
+errors = error_metrics(predictions, testSet.depths, [], testSet, net, netOpts);
 
 % Save results
 fprintf('\nsaving predictions...');
@@ -60,7 +67,7 @@ fprintf('done!\n');
 
 function imdb = get_NYUDepth_v2(opts)
 % -------------------------------------------------------------------------
-% Download required data 
+% Download required data
 % -------------------------------------------------------------------------
 
 opts.dataDir = fullfile(opts.dataDir, 'data');
@@ -82,13 +89,19 @@ if ~exist(filename_splits, 'file')
     websave(filename_splits, url_split);
 end
 
+
 % Load dataset and splits
 fprintf('loading data to workspace...');
 data = load(filename);
-splits = load(filename_splits);
+%data = imread('ta~/jala.jpg');
+%C  C C
+%
+%
+splits = load(filename_splits); %what is split (it's a data set to train the model??)
 
 % Store necessary information to imdb structure
-imdb.images = single(data.images);  %(no mean subtraction has been performed)
+imdb.images = single(data.images); %(no mean subtraction has been performed)
+%
 imdb.depths = single(data.depths);  %depth filled-in values
 imdb.set(splits.trainNdxs) = 1;     %training indices (ignored for inference)
 imdb.set(splits.testNdxs) = 2;      %testing indices (on which evaluation is performed)
@@ -112,4 +125,7 @@ if ~exist(filename, 'file')
 end
 
 net = load(filename);
+
+
+
 
