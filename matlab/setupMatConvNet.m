33e9ef05e7913a1b53e7b24c1a3985ae1d009c21
lpqdao <37049175+lpqdao@users.noreply.github.com>
2018-11-28 00:18:08 -0600

    Update setupMatConvNet.m

--- matlab/setupMatConvNet.m
+++ matlab/setupMatConvNet.m
@@ -2,14 +2,14 @@ function setupMatConvNet(matconvnet_path)
 
 % check path
 if ~exist(fullfile(matconvnet_path, 'matlab', 'vl_setupnn.m'), 'file')
-    error('Count not find MatConvNet in "%s"!\nPlease point matcovnet_path to the correct directory.\n', matconvnet_path);
+    error('Count not find MatConvNet in "%s"!\nPlease point matcovnet_path to the correct directory.\n', matconvnet_pat$
 end
 
 % check if it is the right version (beta-20)
-mcnv = getMatConvNetVersion(matconvnet_path);
-if ~strcmp(mcnv, '1.0-beta20')
-    error('Your MatConvNet version (%s) is not the required version 1.0-beta20. Please download and compile the right version.', mcnv);
-end
+ %mcnv = getMatConvNetVersion(matconvnet_path);
+ %if ~strcmp(mcnv, '1.0-beta25')
+  %  error('Your MatConvNet version (%s) is not the required version 1.0-beta25. Please download and compile the right $
+%end
 
 % if everything is fine, then set up
 run(fullfile(matconvnet_path, 'matlab', 'vl_setupnn.m'));
@@ -24,3 +24,5 @@ fclose(fid);
 idxs = find(~cellfun(@isempty,strfind(s{1}, 'VER = ')));
 mcnVersion = s{1}(idxs(1));
 versionName = mcnVersion{1}(7:end);
+
+
